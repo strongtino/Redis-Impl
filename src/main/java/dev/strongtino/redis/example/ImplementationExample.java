50575573394c8d53d32fe75ace8ffe922d9137c4
strongtino <strongtino.business@gmail.com>
2021-06-06 22:18:22 +0200

    A little string modification

--- src/main/java/dev/strongtino/redis/example/ImplementationExample.java
+++ src/main/java/dev/strongtino/redis/example/ImplementationExample.java
@@ -10,7 +10,7 @@ public class ImplementationExample {
         RedisService service = new RedisService("localhost", 6379);
 
         // Subscribing to the HELLO_WORLD messages
-        service.subscribe(RedisType.HELLO_WORLD, object -> System.out.println("Hello World, " + object.getString("name") + "!"));
+        service.subscribe(RedisType.HELLO_WORLD, object -> System.out.println("Hello World, " + object.getString("name") + '!'));
 
         // Publishing a HELLO_WORLD message
         JsonObject object = new JsonObject();
